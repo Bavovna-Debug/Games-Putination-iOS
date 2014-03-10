533a974efbd365598b37ab9e936594becef4726e
Maxim Shchetynin <maxim@lutz.fritz.box>
2014-03-10 21:36:55 +0100

    Initial Commit

---
+++ Inventorium/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Inventorium
+//
+//  Created by Maxim Shchetynin on 10.03.14.
+//  Copyright (c) 2014 Elena Kukuruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HALAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HALAppDelegate class]));
+    }
+}
